2afe45dd21741f22a72b6c237fbe8be40a6a0158
hopscat <thomas0228@icloud.com>
2026-01-13 23:54:00 -0500

    actually started tutorial work

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-fn main() {
+use std::io;
+fn main()
+{
+    println!("Hey, guess a number:");
+    let mut guessed_number:String = String::new();
+    io::stdin().read_line(&mut guessed_number).expect("Failed to read line");
+    println!("You guessed: {}", guessed_number.trim());
+}
+fn practice() {
     let name = "Tommy";
-    let last = "MyLastName";
+    let last:&str = "MyLastName";
+    //immutable = cannot change
+    //default string I.E let mystring = mytext IS IMUTIBLE CANNOT CHANGE STATIC
+    //to make dynamic let mystring = String::from("my text")
     //only put variable names inside the placeholder {Variable_name} if it is the only thing
     //anything else like .tostring means put var outside placeholder {}, Variable_name
     println!("Hello my full name is, {name} {}!", last.to_lowercase());
 
+    let data = [1,2,3,4,5];
+    println!("{data:?}");
 }
