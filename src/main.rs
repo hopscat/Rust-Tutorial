baea92020fcd749b47906fa8114eaf5c4b15b22c
hopscat <thomas0228@icloud.com>
2026-01-13 23:22:47 -0500

    actually started tutorial work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
-    let name:String = "Tommy".to_string();
-    println!("is this successfully pushing to main?");
+    let name = "Tommy";
+    let last = "MyLastName";
+    //only put variable names inside the placeholder {Variable_name} if it is the only thing
+    //anything else like .tostring means put var outside placeholder {}, Variable_name
+    println!("Hello my full name is, {name} {}!", last.to_lowercase());
+
 }
